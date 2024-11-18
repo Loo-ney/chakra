43529215818d52dda3d43ae3a796663c68a21730
Loo-ney <noe.ralte@gmail.com>
2024-11-18 14:45:39 +0530

    project init

---
+++ server/db.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config(); // Load environment variables from .env file
+
+const connectToDatabase = async () => {
+    try {
+        mongoose.set('strictQuery', false); // Avoid deprecation warnings
+        
+        // Log the value of MONGO_URI to verify it's being loaded correctly
+        console.log('MONGO_URI:', process.env.MONGO_URI);
+
+        const connect = await mongoose.connect(process.env.MONGO_URI,{});
+        
+        console.log(`MongoDb Connected: ${connect.connection.host}`);
+    } catch (error) {
+        console.log(`Error: ${error.message}`);
+        process.exit(1) 
+    }
+}
+
+export default connectToDatabase;
